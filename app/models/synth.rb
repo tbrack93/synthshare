907a4a0ed976bf48e43d5196e2c925f301c15638
Tim Brackfield <Cloud_193@hotmail.com>
2018-06-22 20:05:46 +0100

    added skills

--- app/models/synth.rb
+++ app/models/synth.rb
@@ -12,6 +12,26 @@ class Synth < ApplicationRecord
   validates :build_date, presence: true
   validates :photo, presence: true
 
-
-
+  def skills
+    skills = []
+    skills << "Cleaning" if self.cleaning == true
+    skills << "Cooking" if self.cooking == true
+    skills << "Childcare" if self.childcare == true
+    skills << "Quantum Electrodynamics" if self.quantumelectrodynamics == true
+    skills << "DIY" if self.diy == true
+    skills << "Construction" if self.construction == true
+    skills << "Painting" if self.painting == true
+    skills << "Architectural Engineering" if self.architecturalengineering == true
+    skills << "Gardening" if self.gardening == true
+    skills << "Healthcare" if self.healthcare == true
+    skills << "Elderlycare" if self.elderlycare == true
+    skills << "Animalcare" if self.animalcare == true
+    skills << "Cardiothoracic Surgery" if self.cardiothoracic == true
+    skills << "Driving" if self.driving == true
+    skills << "Self_defense" if self.driving == true
+    skills << "Security" if self.security == true
+    skills << "Juggling" if self.entertainment == true
+    skills << "Hostage Negotiation" if self.hostagenegotiation == true
+    return skills
+  end
 end
