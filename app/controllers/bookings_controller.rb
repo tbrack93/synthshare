b1c631823e1c79e5f7f3d1fcdf74e7f2c87a613d
Tim Brackfield <Cloud_193@hotmail.com>
2018-06-30 22:55:25 +0100

    started on booking confirmation page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,24 +1,31 @@
 class BookingsController < ApplicationController
 before_action :set_synth
+before_action :set_booking, only: [:show]
 
   def create
     @booking = Booking.new(booking_params)
     @booking.user = current_user
     @booking.synth = @synth
-    @booking.price = @synth.price
+    @booking.price = @synth.price * (@booking.end_time - @booking.start_time)
     if @booking.save
-      redirect_to synth_path(@synth)
+      redirect_to synth_booking_path(@synth, @booking)
     else
       render "new"
     end
   end
 
-
+  def show
+    redirect_to @synth unless @booking.user == current_user
+  end
 
   private
 
   def booking_params
     params.require(:booking).permit(:details, :start_time, :end_time)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
   end
 
   def set_synth
