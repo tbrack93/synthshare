bd9bb724e48be569db69d955180c5850dcb6ec85
Tim Brackfield <Cloud_193@hotmail.com>
2018-06-30 18:27:20 +0100

    updated js so past dates validation always reapplied

--- app/javascript/packs/datepick.js
+++ app/javascript/packs/datepick.js
@@ -1,14 +1,18 @@
 const start = document.getElementById("start");
-let end = document.getElementById("end");
 const enddiv = document.getElementById("end-div");
 
 
-end.addEventListener("focus", (event) => {
+function loopevent() {
+ let end = document.getElementById("end");
+ end.addEventListener("focus", (event) => {
   let end = document.getElementById("end");
   let rebirth = '<input data-provide="datepicker" data-date-format="dd-mm-yyyy" data-date-autoclose="true" data-date-start-date="' + start.value +'" autocomplete="off" id="end" type="text" name="booking[end_time]"> <i class="fas fa-calendar-alt"></i>';
   end.remove();
   enddiv.innerHTML = rebirth;
   end = document.getElementById("end");
   end.click();
+  loopevent();
   });
+}
 
+loopevent();
