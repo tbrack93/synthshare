8b0c7964529b1da2b0cef661658df4a99ce5150b
Tim Brackfield <Cloud_193@hotmail.com>
2018-07-07 16:09:51 +0100

    removed dob from user sign up

---
+++ db/migrate/20180707150803_remove_dob_from_users.rb
@@ -0,0 +1,5 @@
+class RemoveDobFromUsers < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :dob, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_04_150825) do
+ActiveRecord::Schema.define(version: 2018_07_07_150803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,7 +90,6 @@ ActiveRecord::Schema.define(version: 2018_07_04_150825) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.date "dob"
     t.string "address"
     t.float "latitude"
     t.float "longitude"
