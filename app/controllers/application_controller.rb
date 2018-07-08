b56fb72ef235a0078204f42d429ee5507a490b5b
Tim Brackfield <Cloud_193@hotmail.com>
2018-07-08 11:45:36 +0100

    added pundit and policy for synths

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_action :authenticate_user!
+  include Pundit
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :verify_authorized, except: :home, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :home, unless: :skip_pundit?
 
    def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :dob, :address])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :disclaimer, :emailconsent])
   end
 
   private
@@ -14,4 +17,9 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     root_path
   end
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
 end
